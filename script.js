c6366c8d6c72bb43b2f97af1ae6fdd02a7caa659
Top-amazon554 <108774065+Top-amazon554@users.noreply.github.com>
2022-07-10 09:43:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var btnme = document.getElementById("btn-menu");
+var span1 = document.getElementById("s1");
+function alerta(){
+		//this.setAttribute("display","none");
+		alert("um viado apertou");
+}
+
+
+
