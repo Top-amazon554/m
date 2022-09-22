ade508f210d03fefc09208412d9bb1c688ace487
Top-amazon554 <108774065+Top-amazon554@users.noreply.github.com>
2022-09-22 12:45:01 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var btnme = document.getElementById("btn-menu");
-var span1 = document.getElementById("s1");
-function alerta(){
-		//this.setAttribute("display","none");
-		alert("um viado apertou");
-}
-
-
-
